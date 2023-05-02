161006a8b7b6300fbaae5b6fbab073e2d53c90d9
Steve Doggett <artigat1@gmail.com>
2023-05-02 09:08:31 +0100

    completed chatbot section of course

---
+++ components/ChatWidget.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { Message, User } from "~~/types";
+
+const me = ref<User>({
+  id: "user", // Roles that match the openai api
+  avatar: "/avatar.jpg",
+  name: "You",
+});
+const bot = ref<User>({
+  id: "assistant",
+  avatar: "/bot.jpg",
+  name: "Botman",
+});
+
+const users = computed(() => [me.value, bot.value]);
+
+const messages = ref<Message[]>([]);
+
+const usersTyping = ref<User[]>([]);
+
+// send messages to Chat API here
+// and in the empty function below
+
+const messagesForApi = computed(() =>
+    messages.value.map((m: Message) =>({
+        role: m.userId,
+        content: m.text,
+    })).slice(-2)
+    // Just send the last 2 messages. Should count words as tokens and probably send the original message as well
+)
+
+async function handleNewMessage(message: Message) {
+    messages.value.push(message)
+    usersTyping.value.push(bot.value)
+    const response: any = await $fetch("/api/ai", {
+        method: "POST",
+        body: {
+            messages: messagesForApi.value,
+        }
+    })
+
+    if(!response.choices[0].message?.content) return
+
+    const msg: Message = {
+      id: response.id,
+        userId: bot.value.id,
+        createdAt: new Date(),
+        text: response.choices[0].message?.content,
+    }
+    messages.value.push(msg)
+    usersTyping.value = []
+}
+
+</script>
+<template>
+  <ChatBox
+    :me="me"
+    :users="users"
+    :messages="messages"
+    @new-message="handleNewMessage"
+    :usersTyping="usersTyping"
+  />
+</template>
